1441a962fb01aa8ff91aab51f6fe1bc89eabbd57
thinsoldier <thinsoldier@thinsoldier.com>
2017-12-24 02:00:46 -0800

    Define ?cmd=abc&id=123 as parts of the route url instead of using the query string.

--- index.php
+++ index.php
@@ -3,10 +3,37 @@ require 'vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
+//-----------------------------
+class AcmeController
+{
+	function processRequest( $request, $response, $args )
+	{
+		ob_start();
+		
+		echo __method__;
+		echo __file__;
+		//var_dump($request->getRequestTarget());
+		//var_dump( get_class_methods($request) );
+		var_dump($args);
+		
+		$response->getBody()->write( ob_get_clean() );
+		
+		// Route callables must return an instance of (Psr\Http\Message\ResponseInterface)
+		return $response;
+	} 
+}
+//-----------------------------
+
 $response = new Zend\Diactoros\Response;
+
+/**
+* See documentation of ServerRequest at:
+* https://github.com/php-fig/http-message/blob/master/src/ServerRequestInterface.php
+*/
 $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
         $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
     	);
+
 $emitter = new Zend\Diactoros\Response\SapiEmitter;
 
 $route = new League\Route\RouteCollection();
@@ -15,6 +42,21 @@ $route->map('GET', '/', function (ServerRequestInterface $request, ResponseInter
     $response->getBody()->write('<h1>Hello, World!</h1>');
 
     return $response;
+});
+
+$route->map('GET', '/orders', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $response->getBody()->write('<h1>Orders Index</h1>');
+    return $response;
+});
+
+
+$route->group('/orders', function ($route) {
+	$route->map('GET' , '/{cmd:new}', 'AcmeController::processRequest');
+	$route->map('POST', '/{cmd:create}', 'AcmeController::processRequest');
+	$route->map('GET' , '/{cmd:edit}/{id:number}', 'AcmeController::processRequest');
+	$route->map('POST', '/{cmd:update}/{id:number}', 'AcmeController::processRequest');
+	$route->map('GET' , '/{cmd:archives}', 'AcmeController::processRequest');
+	$route->map('GET' , '/{cmd:archives}/{page:number}', 'AcmeController::processRequest');
 });
 
 try {
@@ -31,11 +73,9 @@ catch(League\Route\Http\Exception\NotFoundException $exception)
 
 //exit; 
 /*
-cannot see xdebug error messages if the emitter::emit method runs
-because it sends a content length header of only 21 characters
-... which maybe causes the browser to only show that much in source
-... or maybe it's something else happening internally in the emitter??
+Cannot see xdebug error messages if the emitter::emit method runs
+Disable the next line (or exit before it) and look for error messages
+if something isn't working.
 */
 
 $emitter->emit($response);
-
